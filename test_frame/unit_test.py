40d63127b64cd789d1bfa4a513f4ae506b10ff4a
leemiracle <miraclinger@163.com>
2016-09-19 13:10:40 +0800

    有关单元测试

---
+++ test_frame/unit_test.py
@@ -0,0 +1,29 @@
+"""
+unittest:
+1.mock:
+"""
+import unittest
+
+class MockRelate:
+    """
+    引用包：inspect(从python对象中获取信息)
+           pprint(递归的优美的打印：列表，字典，元组)
+           sys(和python解释器强交互)
+           functools(处理函数以及可调用对象)
+        __all__ = (
+        'Mock',
+        'MagicMock',
+        'patch',
+        'sentinel',
+        'DEFAULT',
+        'ANY',
+        'call',
+        'create_autospec',
+        'FILTER_DIR',
+        'NonCallableMock',
+        'NonCallableMagicMock',
+        'mock_open',
+        'PropertyMock',
+        )
+    """
+
