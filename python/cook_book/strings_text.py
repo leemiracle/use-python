fabea95bc30e433410495c44cdb3ca7d169cafb2
leemiracle <miraclinger@163.com>
2016-09-25 09:37:48 +0800

    string

---
+++ python/cook_book/strings_text.py
@@ -0,0 +1,26 @@
+"""
+re:
+str:String object
+fnmatch:
+
+"""
+# 1. Splitting Strings on Any of Multiple Delimiters
+# 2. Matching Text at the Start or End of a String
+# 3. Matching Strings Using Shell Wildcard Patterns
+# 4. Matching and Searching for Text Patterns
+# 5. Searching and Replacing Text
+# 6. Searching and Replacing Case-Insensitive Text
+# 7. Specifying a Regular Expression for the Shortest
+# 8. Writing a Regular Expression for Multiline Patterns
+# 9. Normalizing Unicode Text to a Standard Representation
+# 10. Working with Unicode Characters in Regular Expressions
+# 11. Stripping Unwanted Characters from Strings
+# 12. Sanitizing and Cleaning Up Text
+# 13. Aligning Text Strings
+# 14. Combining and Concatenating Strings
+# 15. Interpolating Variables in Strings
+# 16. Reformatting Text to a Fixed Number of Columns
+# 17. Handling HTML and XML Entities in Text
+# 18. Tokenizing Text
+# 19. Writing a Simple Recursive Descent Parser
+# 20. Performing Text Operations on Byte Strings
