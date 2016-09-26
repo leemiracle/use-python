473ca6cc90ee2f8d0dad193b76360bf77f2c7d5e
leemiracle <miraclinger@163.com>
2016-09-26 10:08:36 +0800

    [ADD]numbers_date_time:大致内容

---
+++ python/cook_book/numbers_dates_times.py
@@ -0,0 +1,16 @@
+# 1. Rounding Numerical Values
+# 2. Performing Accurate Decimal Calculations
+# 3. Formatting Numbers for Output
+# 4. Working with Binary, Octal, and Hexadecimal Integers
+# 5. Packing and Unpacking Large Integers from Bytes
+# 6. Performing Complex-Valued Math
+# 7. Working with Infinity and NaNs
+# 8. Calculating with Fractions
+# 9. Calculating with Large Numerical Arrays
+# 10. Performing Matrix and Linear Algebra Calculations
+# 11. Picking Things at Random
+# 12. Converting Days to Seconds, and Other Basic Time Conversions
+# 13. Determining Last Friday’s Date
+# 14. Finding the Date Range for the Current Month
+# 15. Converting Strings into Datetimes
+# 16. Manipulating Dates Involving Time Zones
