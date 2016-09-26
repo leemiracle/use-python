3c6f18c53b62a0e7c8f1375fa94af878c63394db
leemiracle <miraclinger@163.com>
2016-09-26 11:09:32 +0800

    [ADD]classes_objects:大致内容

---
+++ python/cook_book/classes_objects.py
@@ -0,0 +1,28 @@
+# 1. Changing the String Representation of Instances
+# 2. Customizing String Formatting
+# 3. Making Objects Support the Context-Management Protocol
+# 4. Saving Memory When Creating a Large Number of Instances
+# 5. Encapsulating Names in a Class
+# 6. Creating Managed Attributes
+# 7. Calling a Method on a Parent Class
+# 8. Extending a Property in a Subclass
+# 9. Creating a New Kind of Class or Instance Attribute
+# 10. Using Lazily Computed Properties
+# 11. Simplifying the Initialization of Data Structures
+# 12. Defining an Interface or Abstract Base Class
+# 13. Implementing a Data Model or Type System
+# 14. Implementing Custom Containers
+# 15. Delegating Attribute Access
+# 16. Defining More Than One Constructor in a Class
+# 17. Creating an Instance Without Invoking init
+# 18. Extending Classes with Mixins
+# 19. Implementing Stateful Objects or State Machines
+# 20. Calling a Method on an Object Given the Name As a String
+# 21. Implementing the Visitor Pattern
+# 22. Implementing the Visitor Pattern Without Recursion
+# 23. Managing Memory in Cyclic Data Structures
+# 24. Making Classes Support Comparison Operations
+# 25. Creating Cached Instances
+
+
+
