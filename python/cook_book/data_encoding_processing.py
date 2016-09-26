a2486cd99b380f7103fe09936a2fcbb3add1bace
leemiracle <miraclinger@163.com>
2016-09-26 10:36:07 +0800

    [ADD]data_encoding_processing:大致内容

---
+++ python/cook_book/data_encoding_processing.py
@@ -0,0 +1,13 @@
+# 1. Reading and Writing CSV Data
+# 2. Reading and Writing JSON Data
+# 3. Parsing Simple XML Data
+# 4. Parsing Huge XML Files Incrementally
+# 5. Turning a Dictionary into XML
+# 6. Parsing, Modifying, and Rewriting XML
+# 7. Parsing XML Documents with Namespaces
+# 8. Interacting with a Relational Database
+# 9. Decoding and Encoding Hexadecimal Digits
+# 10. Decoding and Encoding Base64
+# 11. Reading and Writing Binary Arrays of Structures
+# 12. Reading Nested and Variable-Sized Binary
+# 13. Summarizing Data and Performing Statistics
