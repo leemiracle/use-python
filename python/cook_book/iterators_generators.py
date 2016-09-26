a8908ffc3319f5c85691ec541d29b8095f781936
leemiracle <miraclinger@163.com>
2016-09-26 10:19:29 +0800

    [ADD]iterators_generators.py

---
+++ python/cook_book/iterators_generators.py
@@ -0,0 +1,16 @@
+# 1. Manually Consuming an Iterator
+# 2. Delegating Iteration
+# 3. Creating New Iteration Patterns with Generators
+# 4. Implementing the Iterator Protocol
+# 5. Iterating in Reverse
+# 6. Defining Generator Functions with Extra State
+# 7. Taking a Slice of an Iterator
+# 8. Skipping the First Part of an Iterable
+# 9. Iterating Over All Possible Combinations or Permutations
+# 10. Iterating Over the Index-Value Pairs of a Sequence
+# 11. Iterating Over Multiple Sequences Simultaneously(同时)
+# 12. Iterating on Items in Separate Containers
+# 13. Creating Data Processing Pipelines
+# 14. Flattening a Nested Sequence
+# 15. Iterating in Sorted Order Over Merged Sorted Iterables
+# 16. Replacing Infinite while Loops with an Iterator
