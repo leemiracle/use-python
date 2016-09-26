823104c05af559ccb3f4e63f3e36dbc028b323fb
leemiracle <miraclinger@163.com>
2016-09-26 11:37:11 +0800

    [ADD]concurrency:进程

---
+++ python/cook_book/concurrency.py
@@ -0,0 +1,17 @@
+# 1. Starting and Stopping Threads
+# 2. Determining If a Thread Has Started
+# 3. Communicating Between Threads
+# 4. Locking Critical Sections
+# 5. Locking with Deadlock Avoidance
+# 6. Storing Thread-Specific State
+# 7. Creating a Thread Pool
+# 8. Performing Simple Parallel Programming
+# 9. Dealing with the GIL (and How to Stop Worrying About It)
+# 10. Defining an Actor Task
+# 11. Implementing Publish/Subscribe Messaging
+# 12. Using Generators As an Alternative to Threads
+# 13. Polling Multiple Thread Queues
+# 14. Launching a Daemon Process on Unix
+
+
+
