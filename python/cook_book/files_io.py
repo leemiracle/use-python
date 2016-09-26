6db8ddf39373cc3f5c52f642b43ddca11638a3b2
leemiracle <miraclinger@163.com>
2016-09-26 10:31:48 +0800

    [ADD]files_io:大致内容

---
+++ python/cook_book/files_io.py
@@ -0,0 +1,21 @@
+# 1. Reading and Writing Text Data
+# 2. Printing to a File
+# 3. Printing with a Different Separator or Line Ending
+# 4. Reading and Writing Binary Data
+# 5. Writing to a File That Doesn’t Already Exist
+# 6. Performing I/O Operations on a String
+# 7. Reading and Writing Compressed Datafiles
+# 8. Iterating Over Fixed-Sized Records
+# 9. Reading Binary Data into a Mutable Buffer
+# 10. Memory Mapping Binary Files
+# 11. Manipulating Pathnames
+# 12. Testing for the Existence of a File
+# 13. Getting a Directory Listing
+# 14. Bypassing Filename Encoding
+# 15. Printing Bad Filenames
+# 16. Adding or Changing the Encoding of an Already Open File
+# 17. Writing Bytes to a Text File
+# 18. Wrapping an Existing File Descriptor As a File Object
+# 19. Making Temporary Files and Directories
+# 20. Communicating with Serial Ports
+# 21. Serializing Python Objects
