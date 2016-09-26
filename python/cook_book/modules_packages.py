5be224a666119aa1ccc10de735afa1091b73096b
leemiracle <miraclinger@163.com>
2016-09-26 11:23:20 +0800

    [ADD]modules_packages:大致内容

---
+++ python/cook_book/modules_packages.py
@@ -0,0 +1,15 @@
+# 1. Making a Hierarchical Package of Modules
+# 2. Controlling the Import of Everything
+# 3. Importing Package Submodules Using Relative Names
+# 4. Splitting a Module into Multiple Files
+# 5. Making Separate Directories of Code Import Under a Common Namespace
+# 6. Reloading Modules
+# 7. Making a Directory or Zip File Runnable As a Main Script
+# 8. Reading Datafiles Within a Package
+# 9. Adding Directories to sys.path
+# 10. Importing Modules Using a Name Given in a String
+# 11. Loading Modules from a Remote Machine Using Import Hooks
+# 12. Patching Modules on Import
+# 13. Installing Packages Just for Yourself
+# 14. Creating a New Python Environment
+# 15. Distributing Packages
