e933e89a1dc6e1d57a15364faa9c2367390769e5
leemiracle <miraclinger@163.com>
2016-09-26 11:43:06 +0800

    [ADD]system与系统交互

---
+++ python/cook_book/utility_scripting_system_administration.py
@@ -0,0 +1,17 @@
+# 1. Accepting Script Input via Redirection, Pipes, or Input Files
+# 2. Terminating a Program with an Error Message
+# 3. Parsing Command-Line Options
+# 4. Prompting for a Password at Runtime
+# 5. Getting the Terminal Size
+# 6. Executing an External Command and Getting Its Output
+# 7. Copying or Moving Files and Directories
+# 8. Creating and Unpacking Archives
+# 9. Finding Files by Name
+# 10. Reading Configuration Files
+# 11. Adding Logging to Simple Scripts
+# 12. Adding Logging to Libraries
+# 13. Making a Stopwatch Timer
+# 14. Putting Limits on Memory and CPU Usage
+# 15. Launching a Web Browser
+
+
