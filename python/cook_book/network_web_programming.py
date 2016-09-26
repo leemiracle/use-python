510137aa25eae9adf142b05173294933ed37dc77
leemiracle <miraclinger@163.com>
2016-09-26 11:29:47 +0800

    [ADD]network_web_programming:网络编程

---
+++ python/cook_book/network_web_programming.py
@@ -0,0 +1,13 @@
+# 1. Interacting with HTTP Services As a Client
+# 2. Creating a TCP Server
+# 3. Creating a UDP Server
+# 4. Generating a Range of IP Addresses from a CIDR Address
+# 5. Creating a Simple REST-Based Interface
+# 6. Implementing a Simple Remote Procedure Call with XML-RPC
+# 7. Communicating Simply Between Interpreters
+# 8. Implementing Remote Procedure Calls
+# 9. Authenticating Clients Simply
+# 10. Adding SSL to Network Services
+# 11. Passing a Socket File Descriptor Between Processes
+# 12. Understanding Event-Driven I/O
+# 13. Sending and Receiving Large Arrays
