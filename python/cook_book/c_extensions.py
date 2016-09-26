9ac6da7c69d6a4859178e407f482e78f1da6b698
leemiracle <miraclinger@163.com>
2016-09-26 11:53:10 +0800

    [ADD]C_extension:c语言对python的拓展

---
+++ python/cook_book/c_extensions.py
@@ -0,0 +1,23 @@
+# 1. Accessing C Code Using ctypes
+# 2. Writing a Simple C Extension Module
+# 3. Writing an Extension Function That Operates on Arrays
+# 4. Managing Opaque Pointers in C Extension Modules
+# 5. Defining and Exporting C APIs from Extension Modules
+# 6. Calling Python from C
+# 7. Releasing the GIL in C Extensions
+# 8. Mixing Threads from C and Python
+# 9. Wrapping C Code with Swig
+# 10. Wrapping Existing C Code with Cython
+# 11. Using Cython to Write High-Performance Array Operations
+# 12. Turning a Function Pointer into a Callable
+# 13. Passing NULL-Terminated Strings to C Libraries
+# 14. Passing Unicode Strings to C Libraries
+# 15. Converting C Strings to Python
+# 16. Working with C Strings of Dubious Encoding
+# 17. Passing Filenames to C Extensions
+# 18. Passing Open Files to C Extensions
+# 19. Reading File-Like Objects from C
+# 20. Consuming an Iterable from C
+# 21. Diagnosing Segmentation Faults
+
+
