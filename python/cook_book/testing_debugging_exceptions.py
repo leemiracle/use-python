281110eef20c570a3697db4e1f65d70f00131d98
leemiracle <miraclinger@163.com>
2016-09-26 11:47:25 +0800

    test,debug,exception:测试,调试

---
+++ python/cook_book/testing_debugging_exceptions.py
@@ -0,0 +1,15 @@
+# 1. Testing Output Sent to stdout
+# 2. Patching Objects in Unit Tests
+# 3. Testing for Exceptional Conditions in Unit Tests
+# 4. Logging Test Output to a File
+# 5. Skipping or Anticipating Test Failures
+# 6. Handling Multiple Exceptions
+# 7. Catching All Exceptions
+# 8. Creating Custom Exceptions
+# 9. Raising an Exception in Response to Another Exception
+# 10. Reraising the Last Exception
+# 11. Issuing Warning Messages
+# 12. Debugging Basic Program Crashes
+# 13. Profiling and Timing Your Program
+# 14. Making Your Programs Run Faster
+
