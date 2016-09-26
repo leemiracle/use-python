efac440b5c48a1c7dea81e57c05f64b313ea0fb9
leemiracle <miraclinger@163.com>
2016-09-26 10:40:18 +0800

    [ADD]fuctions:大致内容

---
+++ python/cook_book/functions.py
@@ -0,0 +1,12 @@
+# 1. Writing Functions That Accept Any Number of Arguments
+# 2. Writing Functions That Only Accept Keyword Arguments
+# 3. Attaching Informational Metadata to Function Arguments
+# 4. Returning Multiple Values from a Function
+# 5. Defining Functions with Default Arguments
+# 6. Defining Anonymous or Inline Functions
+# 7. Capturing Variables in Anonymous Functions
+# 8. Making an N-Argument Callable Work As a Callable with Fewer Arguments
+# 9. Replacing Single Method Classes with Functions
+# 10. Carrying Extra State with Callback Functions
+# 11. Inlining Callback Functions
+# 12. Accessing Variables Defined Inside a Closure
