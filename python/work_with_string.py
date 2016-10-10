d38c5b18b51dfa04ec8baa0bc1bad6bc1abeca96
leemiracle <miraclinger@163.com>
2016-10-10 12:16:04 +0800

    [ADD]string:unicode to int, int to unicode

--- python/work_with_string.py
+++ python/work_with_string.py
@@ -1,4 +1,15 @@
 
-# 删除字符串中特殊字符
+# 将编码int转换字符
+chr()
+
+# 将字符转换为unicode编码int
+ord()
+
+
 def delete_special_character(string):
+    """
+    删除字符串中特殊字符
+    :param string:
+    :return:
+    """
     return ''.join(e for e in string if e.isalnum())
