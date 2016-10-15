01b92df2b807880b68ac18edd85e931b8fa61137
leemiracle <miraclinger@163.com>
2016-10-15 16:59:21 +0800

    [ADD]状态模式

---
+++ python/design_pattern/state.py
@@ -0,0 +1,85 @@
+"""
+copy for learn
+原版：https://github.com/faif/python-patterns/blob/master/state.py
+"""
+
+
+class State(object):
+
+    def scan(self, different_type, version):
+        """
+        基础状态，共享函数, 操作变量，
+        Scan the dial to the next station
+        """
+        self.pos += 1
+        if self.pos == len(self.stations):
+            self.pos = 0
+        print("Scanning... Station is", self.stations[self.pos], self.name)
+
+
+class AmState(State):
+    """不同状态,不同的变量"""
+
+    def __init__(self, radio):
+        self.radio = radio
+        self.stations = ["1250", "1380", "1510"]
+        self.pos = 0
+        self.name = "AM"
+
+    def toggle_amfm(self):
+        """转换radio的状态"""
+        print("Switching to FM")
+        self.radio.state = self.radio.fmstate
+
+
+class FmState(State):
+
+    def __init__(self, radio):
+        self.radio = radio
+        self.stations = ["81.3", "89.1", "103.9"]
+        self.pos = 0
+        self.name = "FM"
+
+    def toggle_amfm(self):
+        print("Switching to AM")
+        self.radio.state = self.radio.amstate
+
+
+class Radio(object):
+
+    """A radio.     It has a scan button, and an AM/FM toggle switch."""
+
+    def __init__(self):
+        """We have an AM state and an FM state"""
+        self.amstate = AmState(self)
+        self.fmstate = FmState(self)
+        self.state = self.amstate
+
+    def toggle_amfm(self):
+        self.state.toggle_amfm()
+
+    def scan(self):
+        self.state.scan()
+
+
+# Test our radio out
+if __name__ == '__main__':
+    radio = Radio()
+    actions = [radio.scan] * 2 + [radio.toggle_amfm] + [radio.scan] * 2
+    actions *= 2
+
+    for action in actions:
+        action()
+
+### OUTPUT ###
+# Scanning... Station is 1380 AM
+# Scanning... Station is 1510 AM
+# Switching to FM
+# Scanning... Station is 89.1 FM
+# Scanning... Station is 103.9 FM
+# Scanning... Station is 81.3 FM
+# Scanning... Station is 89.1 FM
+# Switching to AM
+# Scanning... Station is 1250 AM
+# Scanning... Station is 1380 AM
+
