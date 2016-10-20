b96855a37c0ea0d71039b934af7fcddf9c3a8e66
leemiracle <miraclinger@163.com>
2016-10-20 17:48:08 +0800

    [ADD]use_function:根据函数属性的不同，函数采取不同的处理流程

--- python/use_function.py
+++ python/use_function.py
@@ -10,16 +10,23 @@ def test_method_1(a):
 
 
 def test_other_method(a):
+    # 根据函数属性的不同，采取不同的处理逻辑
+    if hasattr(test_other_method, 'publish'):
+        print('i have')
     print("do thing")
     test_method_1(a)
     print('do other thing')
 
+
 # test_method 为原函数
 test_method = test_other_method
+test_method_2 = test_other_method
+# 修改函数属性
+test_method_2.publish = 5
 # ----------------
-
 
 if __name__ == '__main__':
     test_method('here')
+    test_method_2('iiiiii')
 
 
