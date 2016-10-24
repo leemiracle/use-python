5afcc1e74c3ebb4dc0eff2ed2906fa397228c505
leemiracle <miraclinger@163.com>
2016-10-24 13:15:13 +0800

    [ADD]elasticsearch install test

--- search/taste_elasticsearch/get_start.py
+++ search/taste_elasticsearch/get_start.py
@@ -6,3 +6,22 @@
 # document:能 被索引 信息 的基本单位
 # shards & replicas:碎片（细分索引为多个 片块， 1.可以分割/缩放内容量.2.可以跨分片[多个节点]分布和并行化操作）
 # 和副本(提供高可用性,扩展搜索量/吞吐量,并行地在所有副本上执行)
+
+# install:java
+# java -version
+# echo $JAVA_HOME
+# https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.1/elasticsearch-2.4.1.deb
+# dpkg -i elasticsearch-2.4.1.deb
+# sudo mkdir -p /usr/share/elasticsearch/config/scripts
+# sudo cp /etc/elasticsearch/elasticsearch.yml /usr/share/elasticsearch/config
+# sudo cp /etc/elasticsearch/elasticsearch.yml /usr/share/elasticsearch/config
+# sudo cp /etc/elasticsearch/logging.yml /usr/share/elasticsearch/config
+# sudo chmod o+rwx /usr/share/elasticsearch/config/elasticsearch.yml /usr/share/elasticsearch/config/logging.yml
+# sudo chmod g+rwx /usr/share/elasticsearch/config/elasticsearch.yml /usr/share/elasticsearch/config/logging.yml
+# sudo mkdir /usr/share/elasticsearch/logs
+# sudo mkdir /usr/share/elasticsearch/data
+# sudo chmod -R 777 /usr/share/elasticsearch/
+
+# 手动执行 elasticsearch
+# /usr/share/elasticsearch/bin/elasticsearch
+
