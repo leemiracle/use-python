4ac6d044027bc1073366c8019dadc333435509bd
leemiracle <miraclinger@163.com>
2016-11-09 17:08:49 +0800

    [add]elasticsearch 搜索api

---
+++ search/taste_elasticsearch/search_apis.py
@@ -0,0 +1,46 @@
+# URI Search
+# Request Body Search
+#   Query
+#   From / Size
+#   Sort
+#   Source filtering
+#   Fields
+#   Script Fields
+#   Doc value Fields
+#   Post filter
+#   Highlighting
+#   Rescoring
+#   Search Type
+#   Scroll
+#   Preference
+#   Explain
+#   Version
+#   Index Boost
+#   min_score
+#   Named Queries
+#   Inner hits
+#   Search After
+
+# Search Template
+# Multi Search Template
+# Search Shards API
+
+# Suggesters
+#   Term suggester
+#   Phrase Suggester
+#   Completion Suggester
+#   Context Suggester
+
+# Multi Search API
+
+# Count API
+
+# Validate API
+
+# Explain API
+
+# Profile配置文件 API
+#   Percolator过滤器
+#   Field stats API
+
+
