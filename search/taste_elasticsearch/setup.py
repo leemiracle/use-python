4576df8f084294a0a4c36cf97c2aa2073a386e08
leemiracle <miraclinger@163.com>
2016-11-09 16:27:04 +0800

    [add]elasticsearch 日常使用

---
+++ search/taste_elasticsearch/setup.py
@@ -0,0 +1,49 @@
+# supported platforms
+
+
+#install
+
+
+# configuration management tools: Puppet, Chef
+# elasticsearch.yml for configuring elasticsearch
+# log4j2.properties for configuring logging
+# config file format
+# environment variable subsitution
+# prompting for settings
+# setting default settings
+
+# logging configuration
+
+
+# bootstrap引导 checks
+
+
+# upgrading升级
+
+
+# stopping 停止
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
