69357c10f6fefcd9972b9904e4ec782e5845bab7
leemiracle <miraclinger@163.com>
2016-11-09 20:22:25 +0800

    [add]elasticsearch  mapping

---
+++ search/taste_elasticsearch/mapping.py
@@ -0,0 +1,57 @@
+# Field datatypes
+#     Array datatype
+#     Binary datatype
+#     Boolean datatype
+#     Date datatype
+#     Geo-point datatype
+#     Geo-Shape datatype
+#     IP datatype
+#     Keyword datatype
+#     Nested datatype
+#     Numeric datatypes
+#     Object datatype
+#     String datatype
+#     Text datatype
+#     Token count datatype
+#     Percolator type
+# Meta-Fields
+#     _all field
+#     _field_names field
+#     _id field
+#     _index field
+#     _meta field
+#     _parent field
+#     _routing field
+#     _source field
+#     _type field
+#     _uid field
+# Mapping parameters
+#     analyzer
+#     boost
+#     coerce
+#     copy_to
+#     doc_values
+#     dynamic
+#     enabled
+#     fielddata
+#     format
+#     ignore_above
+#     ignore_malformed
+#     include_in_all
+#     index
+#     index_options
+#     fields
+#     norms
+#     null_value
+#     position_increment_gap
+#     properties
+#     search_analyzer
+#     similarity
+#     store
+#     term_vector
+# Dynamic Mapping
+#     _default_ mapping
+#     Dynamic field mapping
+#     Dynamic templates
+#     Override default template
+
