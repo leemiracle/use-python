25564f5eaa648a3a1f66a36ba9833289d77739d4
leemiracle <miraclinger@163.com>
2016-11-09 19:43:32 +0800

    [add]elasticsearch 聚合

---
+++ search/taste_elasticsearch/aggregations.py
@@ -0,0 +1,68 @@
+# 聚合
+
+# Metrics度量 Aggregations
+#   Avg Aggregation
+#   Cardinality基数 Aggregation
+#   Extended Stats Aggregation
+#   Geo Bounds边界 Aggregation
+#   Geo Centroid中心 Aggregation
+#   Max Aggregation
+#   Min Aggregation
+#   Percentiles百分位 Aggregation
+#   Percentile Ranks Aggregation
+#   Scripted脚本化 Metric Aggregation
+#   Stats Aggregation
+#   Sum Aggregation
+#   Top hits Aggregation
+#   Value Count Aggregation
+#
+# Bucket桶 Aggregations
+#   Children Aggregation
+#   Date Histogram Aggregation
+#   Date Range Aggregation
+#   Diversified Sampler Aggregation多样化采样器聚合
+#   Filter Aggregation
+#   Filters Aggregation
+#   Geo Distance Aggregation
+#   GeoHash grid Aggregation
+#   Global Aggregation
+#   Histogram直方图 Aggregation
+#   IP Range Aggregation
+#   Missing Aggregation
+#   Nested嵌套 Aggregation
+#   Range Aggregation
+#   Reverse nested Aggregation
+#   Sampler Aggregation
+#   Significant Terms Aggregation
+#   Terms Aggregation
+# Pipeline Aggregations
+#   Avg Bucket Aggregation
+#   Derivative Aggregation
+#   Max Bucket Aggregation
+#   Min Bucket Aggregation
+#   Sum Bucket Aggregation
+#   Stats Bucket Aggregation
+#   Extended Stats Bucket Aggregation
+#   Percentiles Bucket Aggregation
+#   Moving Average Aggregation
+#   Cumulative Sum Aggregation
+#   Bucket Script Aggregation
+#   Bucket Selector Aggregation
+#   Serial Differencing Aggregation
+
+
+# Matrix Aggregations
+#    Matrix Stats
+
+# Caching heavy aggregations
+
+# Returning only aggregation results
+# Aggregation Metadata
+
+
+
+
+
+
+
+
