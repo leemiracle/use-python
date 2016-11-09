b097ee195ee40ab49a7fee80a848d1b919051da6
leemiracle <miraclinger@163.com>
2016-11-09 20:41:13 +0800

    [add]elasticsearch index_modules

---
+++ search/taste_elasticsearch/index_modules.py
@@ -0,0 +1,13 @@
+# Analysis
+# Index Shard Allocation
+#     Shard Allocation Filtering
+#     Delaying allocation when a node leaves
+#     Index recovery prioritization
+#     Total Shards Per Node
+# Mapper
+# Merge
+# Similarity module
+# Slow Log
+# Store
+#     Pre-loading data into the file system cache
+# Translog
