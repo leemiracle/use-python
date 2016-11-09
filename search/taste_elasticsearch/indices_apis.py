7939b5ced2a665a726a2074f7ac8fc89b703d159
leemiracle <miraclinger@163.com>
2016-11-09 19:47:47 +0800

    [add]elasticsearch 索引

---
+++ search/taste_elasticsearch/indices_apis.py
@@ -0,0 +1,28 @@
+# Create Index
+# Delete Index
+# Get Index
+# Indices Exists
+# Open / Close Index API
+# Shrink Index
+# Rollover Index
+# Put Mapping
+# Get Mapping
+# Get Field Mapping
+# Types Exists
+# Index Aliases
+# Update Indices Settings
+# Get Settings
+# Analyze
+#   Explain Analyze
+# Index Templates
+# Shadow replica indices
+#   Node level settings related to shadow replicas
+# Indices Stats
+# Indices Segments
+# Indices Recovery
+# Indices Shard Stores
+# Clear Cache
+# Flush
+#   Synced Flush
+# Refresh
+# Force Merge
