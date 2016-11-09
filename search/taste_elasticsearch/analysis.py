190ebd75812463298646f3beaa1b5b53350c51e0
leemiracle <miraclinger@163.com>
2016-11-09 20:26:45 +0800

    [add]elasticsearch  分析

---
+++ search/taste_elasticsearch/analysis.py
@@ -0,0 +1,73 @@
+# Anatomy of an analyzer
+# Testing analyzers
+# Analyzers
+#     Configuring built-in analyzers
+#     Standard Analyzer
+#     Simple Analyzer
+#     Whitespace Analyzer
+#     Stop Analyzer
+#     Keyword Analyzer
+#     Pattern Analyzer
+#     Language Analyzers
+#     Fingerprint Analyzer
+#     Custom Analyzer
+# Tokenizers
+#     Standard Tokenizer
+#     Letter Tokenizer
+#     Lowercase Tokenizer
+#     Whitespace Analyzer
+#     UAX URL Email Tokenizer
+#     Classic Tokenizer
+#     Thai Tokenizer
+#     NGram Tokenizer
+#     Edge NGram Tokenizer
+#     Keyword Tokenizer
+#     Pattern Tokenizer
+#     Path Hierarchy Tokenizer
+# Token Filters
+#     Standard Token Filter
+#     ASCII Folding Token Filter
+#     Length Token Filter
+#     Lowercase Token Filter
+#     Uppercase Token Filter
+#     NGram Token Filter
+#     Edge NGram Token Filter
+#     Porter Stem Token Filter
+#     Shingle Token Filter
+#     Stop Token Filter
+#     Word Delimiter Token Filter
+#     Stemmer Token Filter
+#     Stemmer Override Token Filter
+#     Keyword Marker Token Filter
+#     Keyword Repeat Token Filter
+#     KStem Token Filter
+#     Snowball Token Filter
+#     Phonetic Token Filter
+#     Synonym Token Filter
+#     Compound Word Token Filter
+#     Reverse Token Filter
+#     Elision Token Filter
+#     Truncate Token Filter
+#     Unique Token Filter
+#     Pattern Capture Token Filter
+#     Pattern Replace Token Filter
+#     Trim Token Filter
+#     Limit Token Count Token Filter
+#     Hunspell Token Filter
+#     Common Grams Token Filter
+#     Normalization Token Filter
+#     CJK Width Token Filter
+#     CJK Bigram Token Filter
+#     Delimited Payload Token Filter
+#     Keep Words Token Filter
+#     Keep Types Token Filter
+#     Classic Token Filter
+#     Apostrophe Token Filter
+#     Decimal Digit Token Filter
+#     Fingerprint Token Filter
+#     Minhash Token Filter
+# Character Filters
+#     HTML Strip Char Filter
+#     Mapping Char Filter
+#     Pattern Replace Char Filter
+
