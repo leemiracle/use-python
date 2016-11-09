ec74528ee18af760388d513294c5ab05993cf795
leemiracle <miraclinger@163.com>
2016-11-09 19:55:01 +0800

    [add]elasticsearch  聚集

---
+++ search/taste_elasticsearch/cluster_apis.py
@@ -0,0 +1,11 @@
+# Cluster Health
+# Cluster State
+# Cluster Stats
+# Pending cluster tasks
+# Cluster Reroute
+# Cluster Update Settings
+# Nodes Stats
+# Nodes Info
+# Task Management API
+# Nodes hot_threads
+# Cluster Allocation Explain API
