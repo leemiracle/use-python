b43e93f7a71257f21d09161226bb3188a6ec3127
leemiracle <miraclinger@163.com>
2016-11-09 16:56:26 +0800

    [add]elasticsearch 文本api

---
+++ search/taste_elasticsearch/document_apis.py
@@ -0,0 +1,79 @@
+# Index API
+
+
+
+# Get API
+
+
+
+
+# Delete API
+
+
+
+
+# Delete By Query API
+
+
+
+
+# Update API
+
+
+
+
+# Update By Query API
+
+
+
+
+# Multi Get API
+
+
+
+
+# Bulk API
+
+
+
+
+# Reindex API
+
+
+
+
+# Term Vectors
+
+
+
+
+# Multi termvectors API
+
+
+
+
+# ?refresh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
