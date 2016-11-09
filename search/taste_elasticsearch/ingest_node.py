349fe5ff22fdc267bd000c3612621c1f56945d6c
leemiracle <miraclinger@163.com>
2016-11-09 20:44:29 +0800

    [add]elasticsearch ingest node

---
+++ search/taste_elasticsearch/ingest_node.py
@@ -0,0 +1,29 @@
+# Pipeline Definition
+# Ingest APIs
+#     Put Pipeline API
+#     Get Pipeline API
+#     Delete Pipeline API
+#     Simulate Pipeline API
+# Accessing Data in Pipelines
+# Handling Failures in Pipelines
+# Processors
+#     Append Processor
+#     Convert Processor
+#     Date Processor
+#     Date Index Name Processor
+#     Fail Processor
+#     Foreach Processor
+#     Grok Processor
+#     Gsub Processor
+#     Join Processor
+#     JSON Processor
+#     Lowercase Processor
+#     Remove Processor
+#     Rename Processor
+#     Script Processor
+#     Set Processor
+#     Split Processor
+#     Sort Processor
+#     Trim Processor
+#     Uppercase Processor
+#     Dot Expander Processor
