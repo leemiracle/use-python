92fdaa08ebfa1118d038735fb16442da04384722
leemiracle <miraclinger@163.com>
2016-11-09 20:29:36 +0800

    [add]elasticsearch  模块

---
+++ search/taste_elasticsearch/modules.py
@@ -0,0 +1,37 @@
+# Cluster
+#     Cluster Level Shard Allocation
+#     Disk-based Shard Allocation
+#     Shard Allocation Awareness
+#     Shard Allocation Filtering
+#     Miscellaneous cluster settings
+# Discovery
+#     Azure Classic Discovery
+#     EC2 Discovery
+#     Google Compute Engine Discovery
+#     Zen Discovery
+# Local Gateway
+# HTTP
+# Indices
+#     Circuit Breaker
+#     Fielddata
+#     Node Query Cache
+#     Indexing Buffer
+#     Shard request cache
+#     Indices Recovery
+# Network Settings
+# Node
+# Plugins
+# Scripting
+#     How to use scripts
+#     Accessing document fields and special variables
+#     Scripting and security
+#     Groovy Scripting Language
+#     Painless Scripting Language
+#     Painless Syntax
+#     Lucene Expressions Language
+#     Native (Java) Scripts
+#     Advanced text scoring in scripts
+# Snapshot And Restore
+# Thread Pool
+# Transport
+# Tribe node
