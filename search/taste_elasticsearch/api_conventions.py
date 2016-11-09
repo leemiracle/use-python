4ae3b2608e18c824479ef773ca7602d6631cd6f0
leemiracle <miraclinger@163.com>
2016-11-09 16:35:09 +0800

    [add]elasticsearch api约定

---
+++ search/taste_elasticsearch/api_conventions.py
@@ -0,0 +1,16 @@
+# api约定
+
+# multiple indices多索引
+# ignore_unavailable
+# allow_no_indices
+# expand_wildcards 展开通配符
+
+
+# date math support in index names
+# static_name
+# date_math_expr
+# date_format
+# time_zone
+
+
+
