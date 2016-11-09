bebb038bd3aba8c89bc462460d383778cdb96049
leemiracle <miraclinger@163.com>
2016-11-09 19:51:29 +0800

    [add]elasticsearch  print on the standard output

---
+++ search/taste_elasticsearch/cat_apis.py
@@ -0,0 +1,20 @@
+# print on the standard output
+
+# cat aliases
+# cat allocation
+# cat count
+# cat fielddata
+# cat health
+# cat indices
+# cat master
+# cat nodeattrs
+# cat nodes
+# cat pending tasks
+# cat plugins
+# cat recovery
+# cat repositories
+# cat thread pool
+# cat shards
+# cat segments
+# cat snapshots
+
