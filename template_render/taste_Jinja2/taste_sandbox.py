8e0845801108c1c2c0cef2a07d4dceb302031fa3
leemiracle <miraclinger@163.com>
2016-11-10 12:01:14 +0800

    [add]沙盒

---
+++ template_render/taste_Jinja2/taste_sandbox.py
@@ -0,0 +1,24 @@
+# Assuming env is a SandboxedEnvironment
+# class jinja2.sandbox.SandboxedEnvironment
+#     call_binop
+#     call_unop
+#     default_binop_table
+#     default_unop_table
+#     intercepted_binops
+#     intercepted_unops
+#     is_safe_attribute
+#     is_safe_callable
+# class jinja2.sandbox.ImmutableSandboxedEnvironment
+# exception jinja2.sandbox.SecurityError
+# jinja2.sandbox.unsafe
+# jinja2.sandbox.is_internal_attribute
+# jinja2.sandbox.modifies_known_mutable
+
+
+
+# Operator Intercepting
+# Environment.call()
+# SandboxedEnvironment.intercepted_binops
+# SandboxedEnvironment.call_binop()
+# SandboxedEnvironment.call_binop
+# SandboxedEnvironment.binop_table
