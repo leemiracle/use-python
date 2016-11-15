6cd0eab55d3ff1146e4c4f58f24d7c0b9cf43d57
leemiracle <miraclinger@163.com>
2016-11-15 16:09:54 +0800

    [add]firesheep 拦截cookie

--- network/taste_hacker/wireless_attack/use_firesheep.py
+++ network/taste_hacker/wireless_attack/use_firesheep.py
@@ -1,3 +1,10 @@
 """
-使用firesheep
+firesheep(Eric Butler):提供了简单的两个按钮接口用来远程窃取不知情用户的为了得到站点的 HTTPCookies 被动的在无线网卡上监听。如果一个用户连接到不安全的网站也没有
+使用任何服务器控件如 HTTPS 来保护他的会话,那么攻击者能使用Firesheep 拦截 cookies 并被重用。
 """
+
+# Session Cookie
+
+# 集结羊群---捕获 WordPress cookie 重用
+
+# 重用来自受害者的 WordPress sessionID
