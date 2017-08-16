81c36f5eeaaa3701070f7e0577354066cd59f62a
leemiracle <miraclinger@163.com>
2017-08-16 20:20:05 +0800

    [add]skip-gram model

--- nerual_work.py
+++ nerual_work.py
@@ -118,10 +118,12 @@ def continuous_bag_of_word(alpha):
     :return:
     """
     # TODO complete input not one-hot model
-    X = np.matrix([[0, 1, 0], [1, 0, 0]])
-    W_k_n = np.matrix(np.random.random((3, 2)))
+    X = np.matrix([[0, 1, 0, 0], [1, 0, 0, 0]])
+    W_k_n = np.matrix(np.random.random((4, 2)))
     W_n_m = np.matrix(np.random.random((2, 4)))
 
+    t = [0 for i in range(X.shape(1))]
+    t[1] = 1
     count = 0
     while 1:
         # in -> hidden
@@ -138,8 +140,7 @@ def continuous_bag_of_word(alpha):
         y_m = softmax(y_m)
         print y_m
 
-        t = [0 for i in range(len(y_m))]
-        t[1] = 1
+
         print "y_m", y_m
 
         # predict result: right to stop
@@ -172,31 +173,60 @@ def continuous_bag_of_word(alpha):
         print "E_i:", E_i
         for i, E in enumerate(E_i):
             W_k_n = W_k_n - np.matmul(E.T, X[i]).T
-        # W_k_n = W_k_n.tolist()
-        # x_k = X.tolist()[0]
-        # for k, w in enumerate(W_k_n):
-        #     for i, a in enumerate(w):
-        #         W_k_n[k][i] = W_k_n[k][i] - alpha * x_k[k] * E_i[i]
         print "W_k_n:", W_k_n
-        # W_k_n = np.matrix(W_k_n)
         W_n_m = np.matrix(W_n_m)
         count += 1
         print "count", count
 
 
 def skip_gram(alpha):
-    pass
+    # TODO complete input not one-hot model
+    X = np.matrix([[0, 1, 0, 0]])
+    W_k_n = np.matrix(np.random.random((4, 2)))
+    W_n_m = np.matrix(np.random.random((2, 4)))
+    count = 0
+    t = np.matrix([[0, 1, 0, 0], [1, 0, 0, 0]])
+    while count < 10:
+        # in(1X4) -> hidden(1X2)
+        h_N = np.matmul(X, W_k_n)
+        # hidden(1X2) -> output(1X4)
+        u_j = np.matmul(h_N, W_n_m)
+        u_j_array = u_j.tolist()[0]
+        y_m = softmax(u_j_array)
+        y_m_verify = [math.floor(2*j) for j in y_m]
+        y_m = np.matrix(y_m)
+
+        # Update equation for hidden→output weights
+        E_j = np.matrix(np.zeros(t.shape[1]))
+        for d in t:
+            E_j = E_j + y_m - d
+        W_n_m = W_n_m - alpha*np.matmul(h_N.T, E_j)
+
+        # Update equation for input→hidden weights
+        E_i = np.matmul(W_n_m, E_j.T).T
+        print "y_m_verify:", y_m_verify
+        print "h_N:", h_N
+        print "W_k_n:", W_k_n
+        print "W_n_m:", W_n_m
+        W_k_n = W_k_n - np.matmul(E_i.T, X).T
+        count += 1
+
 
 def main():
+    """
+    reference:https://arxiv.org/pdf/1411.2738.pdf
+
+    :return:
+    """
     alpha = 0.01
-    sentence = ["abd", "abc", "acabd"]
-    vacab = defaultdict(int)
-    for w in sentence:
-        for c in w:
-            vacab[c] += 1
-    print(vacab)
+    # sentence = ["abd", "abc", "acabd"]
+    # vacab = defaultdict(int)
+    # for w in sentence:
+    #     for c in w:
+    #         vacab[c] += 1
+    # print(vacab)
     # learn_single_unit()
-    continuous_bag_of_word(alpha)
+    skip_gram(alpha)
 
 
 if __name__ == '__main__':
